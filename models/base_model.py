f3889b35375eb1eed31afce2a94e7132935a446a
Tunde <tundeogunmodede1@gmail.com>
2024-03-10 08:37:03 -0700

    new fix

--- models/base_model.py
+++ models/base_model.py
@@ -21,9 +21,9 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict_["created_at"] = datetime.strptime(
+                    self.__dict__["created_at"] = datetime.strptime(
                             kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "upload_at":
+                elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
                             kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
@@ -32,7 +32,7 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-                storage.now(self)
+                storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -43,7 +43,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at"""
 
-        self.Updated_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
