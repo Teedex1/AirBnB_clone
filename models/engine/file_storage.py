d94bc40e9eb2cbea1503f4c2000deb80bf34e984
Tunde <tundeogunmodede1@gmail.com>
2024-03-10 08:33:58 -0700

    new fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ import os
 
 
 class FileStorage:
+
     """Class for storing and retrieving data"""
     __file_path = "file.json"
     __objects = {}
@@ -52,6 +53,7 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
