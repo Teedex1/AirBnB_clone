f01ddacae63293c9d7b26ab68c45310c95826abb
Tunde <tundeogunmodede1@gmail.com>
2024-03-10 07:53:00 -0700

    new fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,7 @@ import os
 
 
 class FileStorage:
-    """Class for storing and retriving data"""
+    """Class for storing and retrieving data"""
     __file_path = "file.json"
     __objects = {}
 
@@ -48,7 +48,7 @@ class FileStorage:
         """Reloads the stored objects"""
         if not os.path.isfile(FileStorage.__file_path):
             return
-        with open(FileStorage.__file_path, "r", encoding="Utf-8") as f:
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
@@ -60,7 +60,7 @@ class FileStorage:
             "BaseModel":
                      {"id": str,
                       "created_at": datetime.datetime,
-                      "update_at": datetime.datetime},
+                      "updated_at": datetime.datetime},
             "User":
                      {"email": str,
                       "password": str,
@@ -79,9 +79,11 @@ class FileStorage:
                       "name": str,
                       "description": str,
                       "number_rooms": int,
+                      "number_bathrooms": int,
                       "max_guest": int,
                       "price_by_night": int,
                       "latitude": float,
+                      "longitude": float,
                       "amenity_ids": list},
             "Review":
             {"place_id": str,
